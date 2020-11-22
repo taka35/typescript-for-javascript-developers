da8ffc00b98c7ba78cdd2932083316357777f009
taka <yutsuha77lps@gmail.com>
2020-11-22 20:04:09 +0900

    add files about arg

---
+++ src/optional-arguments.ts
@@ -0,0 +1,17 @@
+export {};
+
+let bmi: (height: number, weight: number, printable?: boolean) => number = (
+  height: number,
+  weight: number,
+  printable?: boolean
+): number => {
+  const bmi: number = weight / (height * height);
+
+  if (printable) {
+    console.log({ bmi });
+  }
+  return bmi;
+}
+
+bmi(1.72, 65, true);
+
