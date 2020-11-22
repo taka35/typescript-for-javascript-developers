3c6378d2667afd6e14867173a12fb0c23505a9bf
taka <yutsuha77lps@gmail.com>
2020-11-22 14:45:23 +0900

    add enum.ts

---
+++ src/enum.ts
@@ -0,0 +1,35 @@
+export {};
+
+enum Months {
+  January = 1,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+}
+
+console.log(Months.January);
+
+enum Colors {
+  Red = '#FF0000',
+  White = '#FFFFFF',
+  Green = '#008000',
+  Blue = '#0000FF',
+  Black = '#000000'
+};
+
+let green =Colors.Green;
+console.log(green);
+
+enum Colors {
+  Yellow = '#FFFF00'
+}
+
+Colors.Yellow;
