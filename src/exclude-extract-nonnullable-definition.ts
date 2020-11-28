f88a73566d03b35cb3b25a5684123ce785e6e8d7
taka <yutsuha77lps@gmail.com>
2020-11-28 17:35:45 +0900

    add exclude-extract-nonnullable-definition.ts

--- src/exclude-extract-nonnullable-definition.ts
+++ src/exclude-extract-nonnullable-definition.ts
@@ -1,16 +1,14 @@
 export {};
 
-type MyExclude =
-  | (string extends string | number ? never : string)
-  | (number extends string | number ? never : number)
-  | (DebugType extends string | number ? never : DebugType);
-
 type DebugType = () => void;
 type SomeTypes = string | number | DebugType;
 type FunctionType = Exclude<SomeTypes, string | number>;
-type MyFunctionType = MyExclude;
+type NonFunctiontype = Exclude<SomeTypes, DebugType>;
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
 
 type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>
 
 type NullableTypes = string | number | null | undefined;
 type NonNullablreTypes = NonNullable<NullableTypes>
