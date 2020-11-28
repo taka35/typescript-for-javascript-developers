94953643863386b570d8ffd898b0b0b72ba65ea8
taka <yutsuha77lps@gmail.com>
2020-11-28 16:57:29 +0900

    add exclude-extract-nonnullable.ts

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,14 @@
+export {};
+
+type DebugType = () => void;
+type SomeTypes = string | number | DebugType;
+type FunctionType = Exclude<SomeTypes, string | number>;
+type NonFunctiontype = Exclude<SomeTypes, DebugType>;
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
+
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionTypeByExtract = Extract<SomeTypes, string | number>;
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>
+
+type NullableTypes = string | number | null | undefined;
+type NonNullablreTypes = NonNullable<NullableTypes>
